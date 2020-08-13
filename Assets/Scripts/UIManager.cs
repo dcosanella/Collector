3718755e624e8853c6369e1caf976fdf482ae511
dcosanella <dean.cosanella@gmail.com>
2020-08-13 14:19:52 -0700

    Add functionality to save high score

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,14 +6,21 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     private GameManager m_gameManager = null;
+
+    [SerializeField]
+    private Text m_highScoreText;
+
     [SerializeField]
     private Text m_scoreText;
+
     [SerializeField]
     private Text m_gameOverText;
 
     // Start is called before the first frame update
     void Start()
     {
+        LoadHighScore();
+
         m_scoreText.text = "Score: 0";
         m_gameOverText.gameObject.SetActive(false);
 
@@ -33,11 +40,60 @@ public class UIManager : MonoBehaviour
     public void GameOver()
     {
         m_gameOverText.gameObject.SetActive(true);
+
+        SaveHighScore();
+
         m_gameManager.GameOver();
     }
 
     public void UpdateScore(int score)
     {
         m_scoreText.text = "Score: " + score.ToString();
+    }
+
+    private void LoadHighScore()
+    {
+        string highScore = GetHighScore();
+        if (string.IsNullOrEmpty(highScore))
+        {
+            m_highScoreText.gameObject.SetActive(false);
+            return;
+        }
+
+        m_highScoreText.text = "High Score: " + highScore;
+        m_highScoreText.gameObject.SetActive(true);
+    }
+
+    private string GetHighScore()
+    {
+        string score = PlayerPrefs.GetString("HighScore");
+        if (string.IsNullOrEmpty(score))
+        {
+            return null;
+        }
+        
+        return ParseScore(score);
+    }
+
+    private string ParseScore(string score) {
+        var index = score.IndexOf(" ");
+        return score.Substring(++index);
+    }
+
+    private void SaveHighScore()
+    {
+        string highScoreText = GetHighScore();
+        string currentScore = ParseScore(m_scoreText.text);
+
+        if (string.IsNullOrEmpty(highScoreText) || int.Parse(highScoreText) < int.Parse(currentScore))
+        {
+            m_highScoreText.text = "High Score: " + currentScore;
+            if (!m_highScoreText.gameObject.activeInHierarchy)
+            {
+                m_highScoreText.gameObject.SetActive(true);
+            }
+
+            PlayerPrefs.SetString("HighScore", currentScore);
+        }
     }
 }
